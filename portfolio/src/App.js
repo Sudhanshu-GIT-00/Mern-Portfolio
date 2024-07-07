9075d8d0b00af0fc9e767e75fd0f1555571d1284
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-07-08 00:28:20 +0530

    i am creating a portfolio in mern

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Mern portfolio i want push
+    </div>
+  );
+}
+
+export default App;
