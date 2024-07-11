a27d9aebd60e4fdf0e510965e11b220538298dfa
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-07-12 01:00:22 +0530

    now i am working on courses

--- portfolio/src/resorces/courses.js
+++ portfolio/src/resorces/courses.js
@@ -21,6 +21,6 @@ export const courses = [
         _id: "6686b08017660f4159b275c123",
         title: "mern courses",
         image: "https://www.hollywoodreporter.com/wp-content/uploads/2012/12/img_logo_blue.jpg?w=1440&h=810&crop=1",
-        link: "/",
-    },
+        link: "/"
+    }
 ]
