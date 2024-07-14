f81523c71504d517f8dbb0ca2c4f4fd00d987987
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-07-14 19:12:13 +0530

    change leftsider

--- portfolio/src/Pages/Home/LeftSider.js
+++ portfolio/src/Pages/Home/LeftSider.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function LeftSider() {
-  return (
-    <div>LeftSider</div>
-  )
+    return (
+        <div className='fixed left-0 bottom-0 px-10'>
+            <div className="flex flex-col items-center">
+                <div className="flex flex-col gap-3">
+                    <i class="ri-facebook-circle-line text-gray-400 text-xl"></i>
+                    <i class="ri-mail-line text-gray-400 text-xl"></i>
+                    <i class="ri-instagram-line text-gray-400 text-xl"></i>
+                    <i class="ri-linkedin-box-line text-gray-400 text-xl"></i>
+                    <i class="ri-github-line text-gray-400 text-xl"></i>
+                </div>
+                <div className="w-[2px] h-52 bg-[#125f63]">
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LeftSider
