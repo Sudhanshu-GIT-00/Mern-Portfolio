9e10e79d0bf84fd5b2fc0fe9750ec730aff50eb0
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-07-14 22:16:50 +0530

    change leftsider and some deatail

--- portfolio/src/Pages/Home/LeftSider.js
+++ portfolio/src/Pages/Home/LeftSider.js
@@ -5,18 +5,31 @@ function LeftSider() {
         <div className='fixed left-0 bottom-0 px-10'>
             <div className="flex flex-col items-center">
                 <div className="flex flex-col gap-3">
-                    <i class="ri-facebook-circle-line text-gray-400 text-xl"></i>
-                    <i class="ri-mail-line text-gray-400 text-xl"></i>
-                    <i class="ri-instagram-line text-gray-400 text-xl"></i>
-                    <i class="ri-linkedin-box-line text-gray-400 text-xl"></i>
-                    <i class="ri-github-line text-gray-400 text-xl"></i>
+                    <a href="https://www.github.com/">
+                        <i class="ri-facebook-circle-line text-gray-600">
+                        </i>
+                    </a>
+                    <a href="https://www.github.com/">
+                        <i class="ri-mail-line text-gray-600">
+                        </i></a>
+                    <a href="https://www.github.com/">
+                        <i class="ri-instagram-line text-gray-600">
+                        </i>
+                    </a>
+                    <a href="https://www.github.com/">
+                        <i class="ri-linkedin-box-line text-gray-600">
+                        </i>
+                    </a>
+                    <a href="https://www.github.com/">
+                        <i class="ri-github-line text-gray-600">
+                        </i>
+                    </a>
                 </div>
-                <div className="w-[2px] h-52 bg-[#125f63]">
-
+                <div className="w-[2px] h-32 bg-[#125f63]">
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default LeftSider
